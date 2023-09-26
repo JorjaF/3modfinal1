9be19a2b78dbca12eeb00f4d579779f03592bf68
Jorja Fleming <aset284@gmail.com>
2023-09-26 11:16:41 -0600

    Maybe I got this

--- spec/request/munchies_api_spec.rb
+++ spec/request/munchies_api_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 require 'webmock/rspec'
-require 'net/http' 
+require 'net/http'
+require 'uri'
+require 'json'
 
 RSpec.describe "Munchies API", type: :request do
   describe "GET /api/v1/munchies" do
@@ -8,42 +10,58 @@ RSpec.describe "Munchies API", type: :request do
     let(:food) { "italian" }
 
     before do
-      stub_request(:get, "http://api.weatherapi.com/v1/current.json?key=552cd5f4299349deb83155448232409&q=")
+      stub_request(:get, /api.weatherapi.com\/v1\/current.json/)
         .with(
           headers: {
-            'Accept'=>'*/*',
-            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'Host'=>'api.weatherapi.com',
-            'User-Agent'=>'Ruby'
-          })
+            'Accept' => '*/*',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Host' => 'api.weatherapi.com',
+            'User-Agent' => 'Ruby'
+          }
+        )
         .to_return(status: 200, body: "", headers: {})
+            
+      stub_request(:get, "https://api.yelp.com/v3/businesses/search?limit=20&sort_by=best_match")
+        .with(
+          headers: {
+            'Accept' => 'application/json',
+            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Authorization' => 'Bearer YOUR_YELP_API_KEY',
+            'Host' => 'api.yelp.com',
+            'User-Agent' => 'Ruby'
+          }
+        )
+        .to_return(status: 200, body: "", headers: {})
+    end
 
-      stub_request(:get, "http://api.weatherapi.com/v1/current.json?key=552cd5f4299349deb83155448232409&q=pueblo,co")
-
-      stub_request(:get, "https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20")
-
+    it "returns a successful response from Yelp API" do
+      url = URI.parse("https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20")
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
-      
+
       request = Net::HTTP::Get.new(url)
-      request["accept"] = 'application/json'
-      
-      response = http.request(request)
-      puts response.read_body
+      request["Accept"] = "application/json"
+      request["Authorization"] = "Bearer YOUR_YELP_API_KEY" 
 
-      uri = URI("https://api.yelp.com/v3/businesses/search")
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri)
       response = http.request(request)
-      @yelp_response_body = response.body # Capture the response body
+
+      expect(response.code.to_i).to eq(200)
     end
 
-    it "returns a successful response" do
-      expect(response).to have_http_status(:ok)
-    end
+    it "returns the expected JSON response from Yelp API" do
+      stub_request(:get, "https://api.yelp.com/v3/businesses/search?limit=20&sort_by=best_match")
+        .with(
+          headers: {
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer YWCOm_qx25dKgm5Z9I-BVJgC-RPfZIYTYJvLD37Ep0GWJHOHLL_EoyVxWsJAXeFE9P6k3Neho3HhmpbBHIsuqZ-9GDd3hK7le5E9gmdiBHH6pinVzcXEMeH2eQETZXY'
+          }
+        )
+        .to_return(
+          status: 200,
+          body: File.read("spec/fixtures/munchies_response.json"),
+          headers: { 'Content-Type' => 'application/json' }
+        )
 
-    it "returns the expected JSON response" do
       expected_response = {
         "data" => {
           "id" => "null",
@@ -63,8 +81,9 @@ RSpec.describe "Munchies API", type: :request do
           }
         }
       }
+      yelp_response_body = File.read("spec/fixtures/munchies_response.json")
 
-      expect(JSON.parse(@yelp_response_body)).to eq(expected_response)
+      expect(JSON.parse(yelp_response_body)).to eq(expected_response)
     end
   end
 end
